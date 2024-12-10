ed16dc1526230e2170c7f92fd6581798f235211a
timmyd24 <timothy.dav.ns@gmail.com>
2024-12-11 00:33:31 +0800

    updated scripts v6

--- app.py
+++ app.py
@@ -81,132 +81,90 @@ class EventHandler(AssistantEventHandler):
         st.session_state.current_markdown.markdown(format_text, True)
         st.session_state.chat_log.append({"name": "assistant", "msg": format_text})
 
+    @override
+    def on_tool_call_created(self, tool_call):
+        if tool_call.type == "code_interpreter":
+            st.session_state.current_tool_input = ""
+            with st.chat_message("Assistant"):
+                st.session_state.current_tool_input_markdown = st.empty()
 
-def create_thread(content, file):
-    return client.beta.threads.create()
+    @override
+    def on_tool_call_delta(self, delta, snapshot):
+        if 'current_tool_input_markdown' not in st.session_state:
+            with st.chat_message("Assistant"):
+                st.session_state.current_tool_input_markdown = st.empty()
 
+        if delta.type == "code_interpreter":
+            if delta.code_interpreter.input:
+                st.session_state.current_tool_input += delta.code_interpreter.input
+                input_code = f"### code interpreter\ninput:\n```python\n{st.session_state.current_tool_input}\n```"
+                st.session_state.current_tool_input_markdown.markdown(input_code, True)
 
-def create_message(thread, content, file):
-    attachments = []
-    if file is not None:
-        attachments.append(
-            {"file_id": file.id, "tools": [{"type": "code_interpreter"}, {"type": "file_search"}]}
-        )
-    client.beta.threads.messages.create(
-        thread_id=thread.id, role="user", content=content, attachments=attachments
-    )
+            if delta.code_interpreter.outputs:
+                for output in delta.code_interpreter.outputs:
+                    if output.type == "logs":
+                        pass
 
+    @override
+    def on_tool_call_done(self, tool_call):
+        st.session_state.tool_calls.append(tool_call)
+        if tool_call.type == "code_interpreter":
+            if tool_call.id in [x.id for x in st.session_state.tool_calls]:
+                return
+            input_code = f"### code interpreter\ninput:\n```python\n{tool_call.code_interpreter.input}\n```"
+            st.session_state.current_tool_input_markdown.markdown(input_code, True)
+            st.session_state.chat_log.append({"name": "assistant", "msg": input_code})
+            st.session_state.current_tool_input_markdown = None
+            for output in tool_call.code_interpreter.outputs:
+                if output.type == "logs":
+                    output = f"### code interpreter\noutput:\n```\n{output.logs}\n```"
+                    with st.chat_message("Assistant"):
+                        st.markdown(output, True)
+                        st.session_state.chat_log.append(
+                            {"name": "assistant", "msg": output}
+                        )
+        elif (
+            tool_call.type == "function"
+            and self.current_run.status == "requires_action"
+        ):
+            with st.chat_message("Assistant"):
+                msg = f"### Function Calling: {tool_call.function.name}"
+                st.markdown(msg, True)
+                st.session_state.chat_log.append({"name": "assistant", "msg": msg})
+            tool_calls = self.current_run.required_action.submit_tool_outputs.tool_calls
+            tool_outputs = []
+            for submit_tool_call in tool_calls:
+                tool_function_name = submit_tool_call.function.name
+                tool_function_arguments = json.loads(
+                    submit_tool_call.function.arguments
+                )
+                tool_function_output = TOOL_MAP[tool_function_name](
+                    **tool_function_arguments
+                )
+                tool_outputs.append(
+                    {
+                        "tool_call_id": submit_tool_call.id,
+                        "output": tool_function_output,
+                    }
+                )
 
-def create_file_link(file_name, file_id):
-    content = client.files.content(file_id)
-    content_type = content.response.headers["content-type"]
-    b64 = base64.b64encode(content.text.encode(content.encoding)).decode()
-    link_tag = f'<a href="data:{content_type};base64,{b64}" download="{file_name}">Download Link</a>'
-    return link_tag
-
-
-def format_annotation(text):
-    citations = []
-    text_value = text.value
-    for index, annotation in enumerate(text.annotations):
-        text_value = text_value.replace(annotation.text, f" [{index}]")
-    text_value += "\n\n" + "\n".join(citations)
-    return text_value
-
-
-def run_stream(user_input, file, selected_assistant_id):
-    if "thread" not in st.session_state:
-        st.session_state.thread = create_thread(user_input, file)
-    create_message(st.session_state.thread, user_input, file)
-    with client.beta.threads.runs.stream(
-        thread_id=st.session_state.thread.id,
-        assistant_id=selected_assistant_id,
-        event_handler=EventHandler(),
-    ) as stream:
-        stream.until_done()
-
-
-def handle_uploaded_file(uploaded_file):
-    file = client.files.create(file=uploaded_file, purpose="assistants")
-    return file
-
-
-# Define dynamic welcome messages for assistants
-WELCOME_MESSAGES = {
-    "TRITON": (
-        "TRITON is a trial prototype designed to translate plain intentions into coded tactical signals using MTP. "
-        "It is also envisioned as a learning tool to aid personnel in learning how to use MTP for tactical signals. "
-        "Additionally, TRITON is capable of encoding and decoding MTP signals, making it an invaluable tool for tactical communication. "
-        "Joint project by Timothy David, Dean Lee & Tan Chee Wei."
-    ),
-    "GENERAL ASSISTANT (UNCLASSIFIED)": (
-        "Welcome to the General Assistant! This assistant is designed to handle your queries across various topics, "
-        "helping with tasks like research, learning, and document analysis. Let me know how I can assist you today!"
-    ),
-}
-
-
-def reset_welcome_message(selected_assistant_title):
-    """Set the welcome message dynamically based on the selected assistant."""
-    if not st.session_state.chat_log:  # Only set the welcome message if the chat log is empty
-        welcome_msg = WELCOME_MESSAGES.get(selected_assistant_title, "Welcome!")
-        st.session_state.chat_log.append({"name": selected_assistant_title, "msg": welcome_msg})
+            with client.beta.threads.runs.submit_tool_outputs_stream(
+                thread_id=st.session_state.thread.id,
+                run_id=self.current_run.id,
+                tool_outputs=tool_outputs,
+                event_handler=EventHandler(),
+            ) as stream:
+                stream.until_done()
 
 
 def render_chat():
-    """Render chat messages."""
     for chat in st.session_state.chat_log:
         with st.chat_message(chat["name"]):
             st.markdown(chat["msg"], True)
 
 
-def disable_form():
-    st.session_state.in_progress = True
-
-
-def reset_chat():
-    st.session_state.chat_log = []
-    st.session_state.in_progress = False
-
-
-def load_chat_screen(assistant_id, assistant_title):
-    reset_welcome_message(assistant_title)
-    uploaded_file = st.sidebar.file_uploader(
-        enabled_file_upload_message,
-        type=["txt", "pdf", "csv", "json", "geojson", "xlsx", "xls"],
-        disabled=st.session_state.in_progress,
-    )
-    st.title(assistant_title if assistant_title else "")
-    user_msg = st.chat_input("Message", on_submit=disable_form, disabled=st.session_state.in_progress)
-
-    if user_msg:
-        with st.chat_message("user"):
-            st.markdown(user_msg, True)
-        st.session_state.chat_log.append({"name": "user", "msg": user_msg})
-
-        file = None
-        if uploaded_file is not None:
-            file = handle_uploaded_file(uploaded_file)
-
-        # Stream response without rerunning the app
-        run_stream(user_msg, file, assistant_id)
-        st.session_state.in_progress = False
-
-    # Render chat log
-    render_chat()
-
-
 def main():
     multi_agents = os.environ.get("OPENAI_ASSISTANTS", None)
-    single_agent_id = os.environ.get("ASSISTANT_ID", None)
-    single_agent_title = os.environ.get("ASSISTANT_TITLE", "Assistants API UI")
-
-    if authentication_required and "credentials" in st.secrets and authenticator is not None:
-        authenticator.login()
-        if not st.session_state["authentication_status"]:
-            return
-        else:
-            authenticator.logout(location="sidebar")
 
     if multi_agents:
         assistants_json = json.loads(multi_agents)
@@ -216,20 +174,21 @@ def main():
             list(assistants_object.keys()),
             index=None,
             placeholder="Select an assistant profile...",
-            on_change=reset_chat,
+            on_change=lambda: st.session_state.chat_log.clear(),  # Reset chat log on change
         )
         if selected_assistant:
-            selected_assistant_title = assistants_object[selected_assistant]["title"]
-            reset_chat()
-            reset_welcome_message(selected_assistant_title)
-            load_chat_screen(
-                assistants_object[selected_assistant]["id"], selected_assistant_title
+            # Set dynamic welcome message based on selected assistant
+            WELCOME_MESSAGE = (
+                "TRITON is a trial prototype designed to translate plain intentions into coded tactical signals using MTP. "
+                if assistants_object[selected_assistant]["title"] == "TRITON"
+                else "Welcome to the General Assistant! I am here to assist you with general tasks."
             )
-    elif single_agent_id:
-        load_chat_screen(single_agent_id, single_agent_title)
-    else:
-        st.error("No assistant configurations defined in environment variables.")
+            st.session_state.chat_log = [{"name": assistants_object[selected_assistant]["title"], "msg": WELCOME_MESSAGE}]
 
+            load_chat_screen(
+                assistants_object[selected_assistant]["id"],
+                assistants_object[selected_assistant]["title"],
+            )
 
 if __name__ == "__main__":
     main()
